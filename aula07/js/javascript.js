4c0aedb650608387fc587d3b480a3b946342519f
Elvis Silva <elvisfj@gmail.com>
2023-01-29 01:05:45 -0300

    Aula 07 adicionado

---
+++ aula07/js/javascript.js
@@ -0,0 +1,26 @@
+var nome = 'Elvis'
+var sobrenome = 'Silva'
+
+var nomeCompleto = nome + ' ' + sobrenome
+
+console.log(nome)
+console.log(typeof nome)
+
+console.log(typeof 'asdf')
+
+console.log(nomeCompleto)
+
+console.log(typeof '5.272727')
+
+var frase = 'Esta é uma frase complexa'
+
+console.log(frase)
+console.log(typeof frase)
+
+console.log(nome + ' '+ frase)
+
+document.write('Ele disse: "Olá"')
+
+document.write("Ele disse: 'Olá'")
+
+console.log("Este número:" + nome)
