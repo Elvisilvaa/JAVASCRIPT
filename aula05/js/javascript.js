4b75779daaadb4200f8d5753477f6b19f4aafded
Elvis Silva <elvisfj@gmail.com>
2023-01-29 00:44:07 -0300

    Curso de JavaScript

---
+++ aula05/js/javascript.js
@@ -0,0 +1,33 @@
+var teste = 1;
+
+var nome = 'João';
+
+console.log(teste);
+
+teste = 'Matheus';
+
+console.log(teste);
+
+console.log(nome)
+
+var $nome = 'ASD';
+var _nome = 'ASD';
+
+console.log($nome);
+console.log(_nome)
+
+var meuPrimeiroNome = 'Elvis';
+
+console.log(meuPrimeiroNome)
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola)
+
+var meuNome;
+
+meuNome = 'Elvis';
+
+console.log(meuNome)
