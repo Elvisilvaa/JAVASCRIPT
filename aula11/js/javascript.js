0b0fb3ebbd77a27a940ae675e2a91e5d042089f6
Elvis Silva <elvisfj@gmail.com>
2023-02-05 22:22:35 -0300

    Aula 11

---
+++ aula11/js/javascript.js
@@ -0,0 +1,16 @@
+//ARRAYS
+var arr = [5, "Elvis", true, {teste: 1, teste: 2}]; // objeto => {}
+
+console.log(arr)
+
+var arr2 = [2, 3, 4, 5, 6]
+
+console.log(arr2)
+
+console.log(arr[1])
+
+console.log(arr2[0])
+
+arr[4] = 10;
+
+console.log(arr)
