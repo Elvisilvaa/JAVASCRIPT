828cd9246896d2f86b376f244e3d8cc231975d1e
Elvis Silva <elvisfj@gmail.com>
2023-02-05 22:36:32 -0300

    Estudando

--- aula12/js/javascript.js
+++ aula12/js/javascript.js
@@ -1,1 +1,16 @@
 //OPERADORES CONDICIONAIS
+
+var idade = 16
+var idadeMinima = 18
+
+console.log('Antes do if')
+
+if(idade > idadeMinima) {
+    console.log('Pode fazer a carteira de habilitação!')
+}
+
+if(idade > 15) {
+    console.log('Precisa esperar 3 anos ainda para fazer a habilitação!')
+}
+
+console.log('Depois do if')
