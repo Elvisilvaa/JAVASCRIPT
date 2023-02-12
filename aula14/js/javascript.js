9da713292c90f669c353d9d25c00013e9e8fe222
Elvis Silva <elvisfj@gmail.com>
2023-02-12 01:41:39 -0300

    Estudando

---
+++ aula14/js/javascript.js
@@ -0,0 +1,19 @@
+// CONDICIONAIS: ELSE E ELSE IF
+
+var nome = 'Elvis'
+
+if(nome == 'Pedro') {
+    console.log('O nome dele é Pedro')
+} else if(nome == 'Elvis'){
+    console.log('O nome é Elvis')
+}else {
+    console.log('Ele possui outro nome!')
+}
+
+var idade = 18
+
+if(idade >= 18){
+    console.log('Ele pode entrar na festa!')
+}else if (idade <= 17){
+    console.log('Ele não pode entrar na festa!')
+}
