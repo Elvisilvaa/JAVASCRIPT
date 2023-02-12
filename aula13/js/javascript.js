e573e75655b16b4af5cf66562dc2165bd6c900a2
Elvis Silva <elvisfj@gmail.com>
2023-02-12 00:39:28 -0300

    Criado exercício 13

---
+++ aula13/js/javascript.js
@@ -0,0 +1,34 @@
+// OPERADORES DE COMPARAÇÃO
+
+var idade = 18
+var possuiCarro = 1
+
+if(idade >= 18) {
+    console.log('O usuário pode fazer a carteira de motorista!')
+}
+
+if( idade <= 17) {
+    console.log('O usuário não pode fazer a carteira de motorista')
+}
+
+if(possuiCarro) {
+    console.log('O usuário já pode andar de carro!')
+}
+
+var nome = 'Elvis'
+
+if(nome == 'Elvis') {
+    console.log('O seu nome é Elvis')
+}
+
+if(nome != 'Elvis') {
+    console.log('O seu nome não é Elvis')
+}
+
+if(20 > 10) {
+    console.log('Passou')
+}
+
+if (100 < 20) {
+    console.log('Passou 2')
+}
