e7d662c92caaa82fc5677db55a5371869bb172fb
Elvis Silva <elvisfj@gmail.com>
2023-02-18 15:51:00 -0300

    AULA 16

--- aula16/js/javascript.js
+++ aula16/js/javascript.js
@@ -1,1 +1,20 @@
 // OPERADORES LÓGICOS
+
+var idade = 16;
+var nome = 'Elvis';
+
+if(nome == 'Elvis' && idade == 16) {
+    console.log('O Elvis pode entrar na aula de Física');
+} else {
+    console.log('Este não é o Elvis');
+}
+
+if(1 == 1 && 3 > 2 && true) {
+    console.log('Passou');
+}
+
+if((1 == 1 && 3 > 2) && true) {
+    console.log('Passou');
+} else if (nome == 'Elvis' && idade >= 14) {
+    console.log('Aqui passa!');
+}
