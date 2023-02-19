53efa298d86d7b86392a98c7a10cc1294238ab62
Elvis Silva <elvisfj@gmail.com>
2023-02-19 02:22:08 -0300

    Exercício 23

--- aula23/js/javascript.js
+++ aula23/js/javascript.js
@@ -1,2 +1,34 @@
 // FUNÇÕES
 
+function primeiraFuncao() {
+    console.log('Hello World das funções');
+}
+
+primeiraFuncao();
+
+function dizerNome(nome) {
+    console.log('O nome é:' + nome)
+}
+
+dizerNome('Elvis');
+dizerNome('Sabrina');
+dizerNome('Hadassa');
+
+var nomeDoBancoDeDados = 'Pedro';
+
+dizerNome(nomeDoBancoDeDados);
+
+function soma(a, b) {
+    var soma = a + b;
+    return soma;
+}
+
+var somaUm = soma(2, 5);
+
+console.log(somaUm);
+
+var somaDois = soma(5, 5);
+
+console.log(somaDois);
+
+console.log(soma(4, 5));
